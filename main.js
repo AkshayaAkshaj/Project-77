e6c54630e9483ab6706c65e01436b8d118729c85
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2021-10-31 20:47:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name="Shah";
+var middle_name="Rukh";
+var last_name="Khan";
+
+console.log(first_name+middle_name+last_name);
+
+var a = 10;
+var b = 5;
+
+var add= a+b;
+console.log(add);
+
+var subtract=a-b;
+console.log(subtract);
+
+var multiply=a*b;
+console.log(multiply);
+
+var divide=a/b;
+console.log(divide);
